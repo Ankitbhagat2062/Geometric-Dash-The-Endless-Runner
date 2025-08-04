31749a34bc10eaca6b536ed4fa491c5d9bf17090
Ankit Bhagat <ankitbhagat2062@gmail.com>
2025-08-04 10:07:30 +0545

    uploaded

---
+++ Asset.js
@@ -0,0 +1,324 @@
+
+const GENERATED = 1754231212759
+const initOptions = {
+	"width": 480,
+	"height": 360,
+	"stretchStage": false,
+	"fps": 30,
+	"turbo": false,
+	"limits": true,
+	"fencing": true,
+	"pointerLock": false,
+	"autoStart": true,
+	"username": "GeometricDash",
+	"loadingProgress": true,
+	"cloud": {
+		"serverUrl": null,
+		"specialBehaviours": false,
+		"projectId": "276660763"
+	},
+	"extensionWorker": {
+		"url": "https://sheeptester.github.io/scratch-vm/16-9/extension-worker.js"
+	},
+	"extensionCount": 0,
+	"assets": {
+		"project": "./project.json",
+		"b1c6ad5faf3dc74f43a5052552b6635c.png": "./Images/b1c6ad5faf3dc74f43a5052552b6635c.png",
+		"aeb262d51816a416fdc1d999123e434c.png": "./Images/aeb262d51816a416fdc1d999123e434c.png",
+		"afe5237d5c400131a35d78eda6a521fa.png": "./Images/afe5237d5c400131a35d78eda6a521fa.png",
+		"00ca4a0eda82b061b9ad5ed329f6d834.png": "./Images/00ca4a0eda82b061b9ad5ed329f6d834.png",
+		"0f840529b8fe812281603e4ac6a74157.png": "./Images/0f840529b8fe812281603e4ac6a74157.png",
+		"27130e84e8caea2e7ce48c55a6b69c79.png": "./Images/27130e84e8caea2e7ce48c55a6b69c79.png",
+		"f28545318f59797dd925dc7ce64cb16c.png": "./Images/f28545318f59797dd925dc7ce64cb16c.png",
+		"0dde70ee657fde5975b99e09ea4bab3f.png": "./Images/0dde70ee657fde5975b99e09ea4bab3f.png",
+		"25d9b70c2a39765d56c9af1fb95e7c29.wav": "./Music_Sounds/25d9b70c2a39765d56c9af1fb95e7c29.wav",
+		"bd4b2a30968697e48d5d2f703e952692.wav": "./Music_Sounds/bd4b2a30968697e48d5d2f703e952692.wav",
+		"fc65717a786d706245ce43c895558cbd.wav": "./Music_Sounds/fc65717a786d706245ce43c895558cbd.wav",
+		"3a36bf590171c05df5443857c0f5e1ef.wav": "./Music_Sounds/3a36bf590171c05df5443857c0f5e1ef.wav",
+		"3339a2953a3bf62bb80e54ff575dbced.svg": "./Icons/3339a2953a3bf62bb80e54ff575dbced.svg",
+		"83a9787d4cb6f3b7632b4ddfebf74367.wav": "./Music_Sounds/83a9787d4cb6f3b7632b4ddfebf74367.wav",
+		"efa3add2cdbaada1074bf62ae86c9b99.png": "./Images/efa3add2cdbaada1074bf62ae86c9b99.png",
+		"0f52116e60f4ea5a00a756cc6a2f99da.svg": "./Icons/0f52116e60f4ea5a00a756cc6a2f99da.svg",
+		"484b6a2f6a2e1bc0e1b5f895d35ec9d1.svg": "./Icons/484b6a2f6a2e1bc0e1b5f895d35ec9d1.svg",
+		"690f037d6c3c53d6b31660a6f1497674.svg": "./Icons/690f037d6c3c53d6b31660a6f1497674.svg",
+		"01e6a82f20360d97696602b457e32727.wav": "./Music_Sounds/01e6a82f20360d97696602b457e32727.wav",
+		"6dbb64a95dd3c5a652fca3196e886d1f.svg": "./Icons/6dbb64a95dd3c5a652fca3196e886d1f.svg",
+		"022f4dbabee527ad8569bf10c9c34791.svg": "./Icons/022f4dbabee527ad8569bf10c9c34791.svg",
+		"83c36d806dc92327b9e7049a565c6bff.wav": "./Music_Sounds/83c36d806dc92327b9e7049a565c6bff.wav",
+		"d36f6603ec293d2c2198d3ea05109fe0.png": "./Images/d36f6603ec293d2c2198d3ea05109fe0.png",
+		"a78f87869e8707dd49ce43e028c0cb4c.svg": "./Icons/a78f87869e8707dd49ce43e028c0cb4c.svg",
+		"705d7dbc04a4f7ff6cb3def813408454.png": "./Images/705d7dbc04a4f7ff6cb3def813408454.png",
+		"b82ec4dd663cda8823ccdbb0e9bfe970.png": "./Images/b82ec4dd663cda8823ccdbb0e9bfe970.png",
+		"8157a66c4243b3e19bec4f72048eab46.svg": "./Icons/8157a66c4243b3e19bec4f72048eab46.svg",
+		"400277678e8f7ec187b465e34ff77456.svg": "./Icons/400277678e8f7ec187b465e34ff77456.svg",
+		"79363e23b1703bafdd0190f254899e8f.png": "./Images/79363e23b1703bafdd0190f254899e8f.png",
+		"987eee4702336a46de048d0ac48bd3bb.svg": "./Icons/987eee4702336a46de048d0ac48bd3bb.svg",
+		"a183bf2e1ba2e75cb0eece187b0bf7c9.svg": "./Icons/a183bf2e1ba2e75cb0eece187b0bf7c9.svg",
+		"0cb1ee82899b7dcfeefbbd9c9fcf4548.svg": "./Icons/0cb1ee82899b7dcfeefbbd9c9fcf4548.svg",
+		"e5fd76c59c2eb8b1adf76d2287d0b485.svg": "./Icons/e5fd76c59c2eb8b1adf76d2287d0b485.svg",
+		"cb5ef397451f4ad512b46cd676efb988.png": "./Images/cb5ef397451f4ad512b46cd676efb988.png",
+		"4f04e09cc1695ffd4c5ebff564b881db.png": "./Images/4f04e09cc1695ffd4c5ebff564b881db.png",
+		"8f63901fcd11d4d3f4be646f424699bf.png": "./Images/8f63901fcd11d4d3f4be646f424699bf.png",
+		"bfc597853263bf737fde1eda790b5b0a.png": "./Images/bfc597853263bf737fde1eda790b5b0a.png",
+		"d73f58ef7055bd0887699df5c5884735.png": "./Images/d73f58ef7055bd0887699df5c5884735.png",
+		"6b74588189f58d653b9c474d0fb110df.png": "./Images/6b74588189f58d653b9c474d0fb110df.png",
+		"79300d39d7009dcdf81ce745e8cc4d0b.png": "./Images/79300d39d7009dcdf81ce745e8cc4d0b.png",
+		"80df877dd4ef291edbe4a1d40fa81095.png": "./Images/80df877dd4ef291edbe4a1d40fa81095.png",
+		"572320eeeaf916e1611fb5389e349f41.png": "./Images/572320eeeaf916e1611fb5389e349f41.png",
+		"830da2654c8da7cb407025f1f685bcb8.png": "./Images/830da2654c8da7cb407025f1f685bcb8.png",
+		"fc56d5d666e78fac03bdf49c7fe1df00.png": "./Images/fc56d5d666e78fac03bdf49c7fe1df00.png",
+		"6244d95daa373c990aac0b864d0d57a6.png": "./Images/6244d95daa373c990aac0b864d0d57a6.png",
+		"89c0ddd3b4be99ed08a1d53745c9cfce.png": "./Images/89c0ddd3b4be99ed08a1d53745c9cfce.png",
+		"acb7b5040e8284f1459b000c80125010.png": "./Images/acb7b5040e8284f1459b000c80125010.png",
+		"18054eb777d414be9d09463f9b55e2c7.png": "./Images/18054eb777d414be9d09463f9b55e2c7.png",
+		"2e4d432de26ed0b55027f9140da6c9b4.png": "./Images/2e4d432de26ed0b55027f9140da6c9b4.png",
+		"95565ae318e4f4a399ab25564cf42908.png": "./Images/95565ae318e4f4a399ab25564cf42908.png",
+		"aab2f879a615502c95d13cabf33f7f62.png": "./Images/aab2f879a615502c95d13cabf33f7f62.png",
+		"948f06697efc484e5378357124077d0d.png": "./Images/948f06697efc484e5378357124077d0d.png",
+		"4d2640bb6c51ea19fb8e6bfae6381f01.svg": "./Icons/4d2640bb6c51ea19fb8e6bfae6381f01.svg",
+		"d5a2603f8293b4b0bdcd5572ca546403.png": "./Images/d5a2603f8293b4b0bdcd5572ca546403.png",
+		"d4f44007055e1e85660c920e35662f2f.png": "./Images/d4f44007055e1e85660c920e35662f2f.png",
+		"8529deac86f1a492e01e1eb07d6966b7.png": "./Images/8529deac86f1a492e01e1eb07d6966b7.png",
+		"00c79bfce2f93cf705847f55a26a7797.png": "./Images/00c79bfce2f93cf705847f55a26a7797.png",
+		"20e5413122645a63aea7fa699650d7df.png": "./Images/20e5413122645a63aea7fa699650d7df.png",
+		"cd867d882d893326d63862a03376dd99.svg": "./Icons/cd867d882d893326d63862a03376dd99.svg",
+		"263163cf45904179fe8a0447c7b4532a.png": "./Images/263163cf45904179fe8a0447c7b4532a.png",
+		"bb9c2c511c6b22fc55830bec0e1aa84f.png": "./Images/bb9c2c511c6b22fc55830bec0e1aa84f.png",
+		"0d87e741e5752ae4160a0ba5e6832127.png": "./Images/0d87e741e5752ae4160a0ba5e6832127.png",
+		"452a26472d12194ff61de56dceb2d017.svg": "./Icons/452a26472d12194ff61de56dceb2d017.svg",
+		"88d07bbbb4228aa15b6b670a356bc800.svg": "./Icons/88d07bbbb4228aa15b6b670a356bc800.svg",
+		"f0583bb3537877fe3dfa37133a126094.svg": "./Icons/f0583bb3537877fe3dfa37133a126094.svg",
+		"25259e93a0c7aa082fcfa1fb8b83f1cc.svg": "./Icons/25259e93a0c7aa082fcfa1fb8b83f1cc.svg",
+		"bcbf77f2e98f51824b09f821bcd03e46.svg": "./Icons/bcbf77f2e98f51824b09f821bcd03e46.svg",
+		"6803397bc73d1bbd6f57df7cd8f81aac.svg": "./Icons/6803397bc73d1bbd6f57df7cd8f81aac.svg",
+		"f33d1a26058019bfca68863748623d68.svg": "./Icons/f33d1a26058019bfca68863748623d68.svg",
+		"45fa40220bca74c01e102740cbae7d30.svg": "./Icons/45fa40220bca74c01e102740cbae7d30.svg",
+		"d7fc33f142a825b069d3a9cbc7750e2e.svg": "./Icons/d7fc33f142a825b069d3a9cbc7750e2e.svg",
+		"3573670a9d591a537e2547b2ee3aae0c.svg": "./Icons/3573670a9d591a537e2547b2ee3aae0c.svg",
+		"83b509173a8f4137c304a2d2c56c4728.svg": "./Icons/83b509173a8f4137c304a2d2c56c4728.svg",
+		"6df19fce8f1a3e19142a08d940c37dbd.svg": "./Icons/6df19fce8f1a3e19142a08d940c37dbd.svg",
+		"25a9f173b4afc6c66f431f00381688e7.svg": "./Icons/25a9f173b4afc6c66f431f00381688e7.svg",
+		"0f174194c572268f45aa812522095b48.svg": "./Icons/0f174194c572268f45aa812522095b48.svg",
+		"5f76e14c268c8da3158be62acdf4246b.svg": "./Icons/5f76e14c268c8da3158be62acdf4246b.svg",
+		"7be89af63ff3cee2cf00031bc5bfc136.svg": "./Icons/7be89af63ff3cee2cf00031bc5bfc136.svg",
+		"c41faf93ebe113d363a82515e24f55a1.svg": "./Icons/c41faf93ebe113d363a82515e24f55a1.svg",
+		"9be46f5784fd08f0b79ad0efb6175cd2.svg": "./Icons/9be46f5784fd08f0b79ad0efb6175cd2.svg",
+		"2d74b22290d5fa3d4af12478946fe908.png": "./Images/2d74b22290d5fa3d4af12478946fe908.png",
+		"15e3bb98b95abd95358fb69c5e1ca183.png": "./Images/15e3bb98b95abd95358fb69c5e1ca183.png",
+		"fa8acc6a45d187a9cd1c2bc67e61556c.png": "./Images/fa8acc6a45d187a9cd1c2bc67e61556c.png",
+		"8da9342f437ff807e5367a657e4ff31b.png": "./Images/8da9342f437ff807e5367a657e4ff31b.png",
+		"77290149b17f7f7b8a72e24801e4c461.png": "./Images/77290149b17f7f7b8a72e24801e4c461.png",
+		"0de080859bd7897226b704effe85438b.png": "./Images/0de080859bd7897226b704effe85438b.png",
+		"bb92285d92efb241905186f656918492.png": "./Images/bb92285d92efb241905186f656918492.png",
+		"2f232d6e38fa8ad34b76f736ba266833.png": "./Images/2f232d6e38fa8ad34b76f736ba266833.png",
+		"b14e26adb335f5d60885d042ac6ed50c.png": "./Images/b14e26adb335f5d60885d042ac6ed50c.png",
+		"494a195d2487650a1ae64d8c8a5c2738.png": "./Images/494a195d2487650a1ae64d8c8a5c2738.png",
+		"eed0bcf2bf3193aa67dc91ac49a68056.png": "./Images/eed0bcf2bf3193aa67dc91ac49a68056.png",
+		"d03fc9ed71852f942194d527ca83b14b.png": "./Images/d03fc9ed71852f942194d527ca83b14b.png",
+		"1f5501fbf229de36bd239420ca711734.png": "./Images/1f5501fbf229de36bd239420ca711734.png",
+		"0cd10b25cbcb662427cded27f586be39.png": "./Images/0cd10b25cbcb662427cded27f586be39.png",
+		"c49a51d16cb0505d0aa999eb1c4d2df7.png": "./Images/c49a51d16cb0505d0aa999eb1c4d2df7.png",
+		"d1267d8d4cfce4af3280f2779f4bfcdb.svg": "./Icons/d1267d8d4cfce4af3280f2779f4bfcdb.svg",
+		"5bc65e9d1c399f81983b424050b23d46.png": "./Images/5bc65e9d1c399f81983b424050b23d46.png",
+		"0865b435dc4ed157dfed909a8fd35165.png": "./Images/0865b435dc4ed157dfed909a8fd35165.png",
+		"9470290f5217e7032a274ed3cdd759dc.png": "./Images/9470290f5217e7032a274ed3cdd759dc.png",
+		"ca822a3bce69e9dc8549764ff723c7aa.png": "./Images/ca822a3bce69e9dc8549764ff723c7aa.png",
+		"a33808ab0d099a345cafe4d105e1fc73.png": "./Images/a33808ab0d099a345cafe4d105e1fc73.png",
+		"f64945b3e693a2389c1e5520c4689b5e.svg": "./Icons/f64945b3e693a2389c1e5520c4689b5e.svg",
+		"36520209bcc147e28840461cc6611542.svg": "./Icons/36520209bcc147e28840461cc6611542.svg",
+		"8cfdaf476a5d81fd6caf97b3b61c99e6.svg": "./Icons/8cfdaf476a5d81fd6caf97b3b61c99e6.svg",
+		"40660bc91bf43309d6477ff4c2112bb2.svg": "./Icons/40660bc91bf43309d6477ff4c2112bb2.svg",
+		"ad19167c16adaa6e3c4d3020fe32003a.svg": "./Icons/ad19167c16adaa6e3c4d3020fe32003a.svg",
+		"360e306cfda30f6b16cac9ad6592eab0.svg": "./Icons/360e306cfda30f6b16cac9ad6592eab0.svg",
+		"716807f3bbda366b53236e5bc848bfa8.svg": "./Icons/716807f3bbda366b53236e5bc848bfa8.svg",
+		"bb3bde63e4e6bf132b79aafdee3f61b5.png": "./Images/bb3bde63e4e6bf132b79aafdee3f61b5.png",
+		"71d045125eae1d971d9075cf17538d47.svg": "./Icons/71d045125eae1d971d9075cf17538d47.svg",
+		"571997a4af3d0439b3ebcb7dc3678690.svg": "./Icons/571997a4af3d0439b3ebcb7dc3678690.svg",
+		"0ca6bc0d9761fc7561519cc2b2a83a4e.svg": "./Icons/0ca6bc0d9761fc7561519cc2b2a83a4e.svg",
+		"37e445c83c56df6a4d672073cb80ecd4.svg": "./Icons/37e445c83c56df6a4d672073cb80ecd4.svg",
+		"6bfed56176103c3366cea4ff0f72141e.svg": "./Icons/6bfed56176103c3366cea4ff0f72141e.svg",
+		"9cec716c723d665e00e171addcf42109.svg": "./Icons/9cec716c723d665e00e171addcf42109.svg",
+		"0a9e1bd7dc5b8be77f9e5844cb9bacd8.svg": "./Icons/0a9e1bd7dc5b8be77f9e5844cb9bacd8.svg",
+		"b330c716329414a7a107a0303a49811f.svg": "./Icons/b330c716329414a7a107a0303a49811f.svg",
+		"4aa14ea54ae0d509b0a216b90d37aac9.svg": "./Icons/4aa14ea54ae0d509b0a216b90d37aac9.svg",
+		"b461df622e2ac94cf5ff8936c5e8d67d.svg": "./Icons/b461df622e2ac94cf5ff8936c5e8d67d.svg",
+		"37a7f7988bd42aa4a8fbcdede12f0871.svg": "./Icons/37a7f7988bd42aa4a8fbcdede12f0871.svg",
+		"2177774d92dc35750262b544e58238a3.svg": "./Icons/2177774d92dc35750262b544e58238a3.svg",
+		"38dd279a5f09878d88f840e89fa8f836.svg": "./Icons/38dd279a5f09878d88f840e89fa8f836.svg",
+		"4821ed41bb9bca70825cca9c18d47ef5.svg": "./Icons/4821ed41bb9bca70825cca9c18d47ef5.svg",
+		"240007258028a4cd2b938cc7cdb7b1d4.svg": "./Icons/240007258028a4cd2b938cc7cdb7b1d4.svg",
+		"5c5ee3d68800d785b623daf74dca631b.svg": "./Icons/5c5ee3d68800d785b623daf74dca631b.svg",
+		"3ccd31c75f1df4c3ca0e607f7982cabb.svg": "./Icons/3ccd31c75f1df4c3ca0e607f7982cabb.svg",
+		"bbbca7c6fe7006d2af8a4b8ec000c7bd.png": "./Images/bbbca7c6fe7006d2af8a4b8ec000c7bd.png",
+		"9f2d1617c328e1b22296297d3c3bb7ce.png": "./Images/9f2d1617c328e1b22296297d3c3bb7ce.png",
+		"f04c9165135542a591a4b05567db63f3.png": "./Images/f04c9165135542a591a4b05567db63f3.png",
+		"26359865f5e82145e55c37385353171a.svg": "./Icons/26359865f5e82145e55c37385353171a.svg",
+		"0c3ebcaf3bac2e0be97e0393673869db.svg": "./Icons/0c3ebcaf3bac2e0be97e0393673869db.svg",
+		"151ebc6f6cec8802d7f42e8f66c5117d.svg": "./Icons/151ebc6f6cec8802d7f42e8f66c5117d.svg",
+		"e71a58dc7a58078b63137b1518e2e7d1.svg": "./Icons/e71a58dc7a58078b63137b1518e2e7d1.svg",
+		"c2aadfdf28f8a32c9dd8e056db555cd3.svg": "./Icons/c2aadfdf28f8a32c9dd8e056db555cd3.svg",
+		"01572df998be7fc3d5ff81f2411f5dc6.svg": "./Icons/01572df998be7fc3d5ff81f2411f5dc6.svg",
+		"85f42d8e142b2c4311d884fc9497b08b.svg": "./Icons/85f42d8e142b2c4311d884fc9497b08b.svg",
+		"8b2112bc7510870649b8f39c9cf3f700.svg": "./Icons/8b2112bc7510870649b8f39c9cf3f700.svg",
+		"d4a55922a6ae924390949ff3790db86e.png": "./Images/d4a55922a6ae924390949ff3790db86e.png",
+		"fe932b1695756f240d1a06205c6f5e8b.svg": "./Icons/fe932b1695756f240d1a06205c6f5e8b.svg",
+		"f5a5715bfdd7ca573898212d222c31b4.svg": "./Icons/f5a5715bfdd7ca573898212d222c31b4.svg",
+		"f5e250886e3bafbbd9bc2398a5e321c5.png": "./Images/f5e250886e3bafbbd9bc2398a5e321c5.png",
+		"5563edc8e03ea17200e941ab1682d348.svg": "./Icons/5563edc8e03ea17200e941ab1682d348.svg",
+		"436262afb26c0cc06866b1e80b6fe0a3.svg": "./Icons/436262afb26c0cc06866b1e80b6fe0a3.svg",
+		"a75b59a3e8673f28fcfd76d7ee208740.svg": "./Icons/a75b59a3e8673f28fcfd76d7ee208740.svg",
+		"d238b9d1112377bf331a97d3eba02282.svg": "./Icons/d238b9d1112377bf331a97d3eba02282.svg",
+		"e7d52f4f6b777792f87580e96c04698e.svg": "./Icons/e7d52f4f6b777792f87580e96c04698e.svg",
+		"f64afb50c0358946a4f27fa662228036.svg": "./Icons/f64afb50c0358946a4f27fa662228036.svg",
+		"4a6ae7ae2e4631100deb49d89e008c55.png": "./Images/4a6ae7ae2e4631100deb49d89e008c55.png"
+	
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  }
+}
+init(initOptions)
---
+++ README.md
@@ -0,0 +1,126 @@
+# Geometric Dash Game
+
+![Geometric Dash Game](Images/0f840529b8fe81228866b1518e2e7d1.png)
+
+## Overview
+
+This is a clone of the popular **Geometric Dash** game created as part of my CS50 Introduction to Computer Science course assignment. The game was built using **Scratch**, a visual programming language designed for kids, and then converted to HTML using **Htmlifier** for web deployment.
+
+## About CS50
+
+This project was completed as part of **Week 0** of [CS50: Introduction to Computer Science](https://pll.harvard.edu/course/cs50-introduction-computer-science) from Harvard University. The assignment focused on introducing students to computational thinking through visual programming.
+
+## Project Details
+
+- **Assignment Grade**: 2/2 (Full Marks)
+- **Original Scratch Project**: [Geometric Dash on Scratch](https://scratch.mit.edu/projects/1201934945)
+- **GitHub Repository**: [Geometric-Dash on GitHub](https://github.com/Ankitbhagat2062/Geometric-Dash)
+- **Live Demo**: [Play Geometric Dash](https://result-7souvf1jk-ankitbhagat2062s-projects.vercel.app)
+
+## About Geometric Dash
+
+Geometric Dash is a popular rhythm-based platformer game where players control a cube that jumps and moves automatically. The objective is to navigate through levels filled with obstacles synchronized to the beat of the music without colliding with any obstacles.
+
+## How It Was Made
+
+### 1. Scratch Development
+- Created using [Scratch](https://scratch.mit.edu/), a block-based visual programming language
+- Designed sprites, backgrounds, and game mechanics using Scratch's intuitive interface
+- Implemented physics, collision detection, and scoring systems using visual code blocks
+
+### 2. HTML Conversion
+- Converted from Scratch to HTML using [Htmlifier](https://sheeptester.github.io/htmlifier/)
+- Htmlifier packages Scratch projects into standalone HTML files that can run in web browsers
+- Preserves all game functionality while making it web-compatible
+
+### 3. Deployment
+- Uploaded to GitHub for version control
+- Hosted on Vercel for public access
+
+## How to Play
+
+1. **Controls**: 
+   - Press the **spacebar** or **up arrow** to make the character jump
+   - Click the green flag to start the game
+   - Click the red stop sign to reset the game
+
+2. **Objective**:
+   - Navigate through the level without hitting obstacles
+   - Try to achieve the highest score possible
+   - Each successful pass through an obstacle increases your score
+
+3. **Features**:
+   - Physics-based movement
+   - Score tracking
+   - Multiple levels of increasing difficulty
+   - Synchronized music and gameplay
+
+## Technical Information
+
+### Project Structure
+```
+├── index.html          # Main game file
+├── CSS/
+│   └── style.css       # Game styling
+├── JS/
+│   ├── main.js         # Game logic
+│   ├── vm.js           # Virtual machine
+│   └── Error.js        # Error handling
+├── Images/             # Game assets
+├── Icons/              # UI icons
+└── Music_Sounds/       # Audio files
+```
+
+### Technologies Used
+- **Scratch**: Visual programming language
+- **JavaScript**: Game logic and interactions
+- **HTML/CSS**: Structure and styling
+- **Htmlifier**: Scratch to HTML conversion tool
+
+## Development Process
+
+1. **Design Phase**: 
+   - Planned game mechanics and level design
+   - Created sprites and backgrounds in Scratch
+
+2. **Implementation Phase**:
+   - Programmed character movement and physics
+   - Implemented collision detection
+   - Added scoring system and UI elements
+
+3. **Testing Phase**:
+   - Tested gameplay mechanics
+   - Adjusted difficulty and timing
+   - Verified scoring system
+
+4. **Deployment Phase**:
+   - Converted to HTML using Htmlifier
+   - Uploaded to GitHub repository
+   - Deployed to Vercel for public access
+
+## Learning Outcomes
+
+This project helped me understand:
+- Visual programming concepts
+- Game design principles
+- Event-driven programming
+- Basic physics implementation
+- Web deployment processes
+
+## Play the Game
+
+You can play the game in several ways:
+
+1. **Online**: Visit the [live demo](https://result-7souvf1jk-ankitbhagat2062s-projects.vercel.app)
+2. **Locally**: Download the repository and open `index.html` in your browser
+3. **Scratch**: View the original project on [Scratch](https://scratch.mit.edu/projects/1201934945)
+
+## Acknowledgements
+
+- [Scratch Team](https://scratch.mit.edu/) for creating an accessible programming platform
+- [Htmlifier](https://sheeptester.github.io/htmlifier/) for the Scratch to HTML conversion tool
+- [CS50 Team](https://cs50.harvard.edu/) for the educational opportunity
+
+## License
+
+This project is for educational purposes as part of the CS50 course requirements.
